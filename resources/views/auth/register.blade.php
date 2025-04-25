7c10e0423a85eb8a06b98cef141265c101dd0c08
Geoffrey Canivet <geoffrey.canivet@gmail.com>
2025-04-25 22:41:17 +0200

    maj

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,45 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+@extends('auth.auth-layout')
+
+@section('title', "Inscription")
+
+@section('auth-form')
+
+    <h1 class="mb-3">S'inscrire</h1>
+    <form action="{{ route('register') }}" method="POST">
         @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        <div class="form-group">
+            <input class="form-control" type="text" name="name" value="{{ old('name') }}" placeholder="Nom">
+            @error('name')
+                <p class="text-red-500 mt-2">{{ $message }}</p>
+            @enderror
         </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div class="form-group">
+            <input class="form-control" type="email" name="email" value="{{ old('email') }}" placeholder="Email">
+            @error('email')
+                <p class="text-red-500 mt-2">{{ $message }}</p>
+            @enderror
         </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        <div class="form-group">
+            <input class="form-control" type="password" name="password" placeholder="Mot de passe">
+            @error('password')
+            <p class="text-red-500 mt-2">{{ $message }}</p>
+            @enderror
         </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        <div class="form-group">
+            <input class="form-control" type="password" name="password_confirmation" placeholder="Confirmer Mot de passe">
+            @error('password_confirmation')
+            <p class="text-red-500 mt-2">{{ $message }}</p>
+            @enderror
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+        <div class="form-group mb-0">
+            <button class="btn btn-primary btn-block" type="submit">S'inscrire</button>
         </div>
     </form>
-</x-guest-layout>
+
+    <div class="text-center dont-have">Vous avez déjà un compte ? <a href="{{ route('login') }}">Se connecter</a></div>
+
+@endsection
---
+++ resources/views/back/partials/stripts.blade.php
@@ -0,0 +1,12 @@
+<script
+    data-cfasync="false"
+    src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"
+></script>
+<script src="{{asset('back_auth/assets/js/jquery-3.5.1.min.js')}}"></script>
+<script src="{{asset('back_auth/assets/js/popper.min.js')}}"></script>
+<script src="{{asset('back_auth/assets/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('back_auth/assets/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
+<script src="{{asset('back_auth/assets/plugins/raphael/raphael.min.js')}}"></script>
+<script src="{{asset('back_auth/assets/plugins/morris/morris.min.js')}}"></script>
+<script src="{{asset('back_auth/assets/js/chart.morris.js')}}"></script>
+<script src="{{asset('back_auth/assets/js/script.js')}}"></script>
